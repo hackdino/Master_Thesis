67f2604f52ca4cf7504842f2a6383871b6a07a50
hackdino <franzney14@gmail.com>
2018-09-04 18:17:10 +0200

    04082018

---
+++ Code_Samples/Klassen/Beispiel_V3/serial.h
@@ -0,0 +1,22 @@
+class Serial {
+  
+  public:
+  // ctors
+  Serial():Serial(9600, 1){};
+  Serial(int _baud, int _mode);
+  ~Serial();
+  
+  void set_baudrate(int baudate);
+  void set_mode(int mode);
+  
+  private:
+  int baudrate;
+  int mode;
+  char *buffer;
+  
+};
+
+
+
+
+/*EOF*/
