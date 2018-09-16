d512eb09b7080082564d63c812af082b06cba0b4
hackdino <franzney14@gmail.com>
2018-09-16 14:03:21 +0200

    16092018

---
+++ Code_Samples/Operatoren_Ueberladen/new/main.cpp
@@ -0,0 +1,26 @@
+class Class {
+  
+  public:
+  int a{0};
+  int b{0};
+   
+  Class operator+(const Class& val) {
+    Class tmp;
+    tmp.a = a + val.a;
+    tmp.b = b + val.b;
+    return tmp;
+  }
+};
+
+int main(void){
+  
+  Class class_a, class_b;
+  Class class_c;
+  
+  class_a.a = 2;
+  class_b.b = 5;
+  
+  class_c = class_a + class_b;
+  
+  return 1;
+}
